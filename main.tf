896f73fa755d60c62b0ce5a4c1d112f6c785bfb4
StevenJohnWeaver <steve.weaver@hashicorp.com>
2026-02-12 09:34:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-terraform {}
+terraform {
+  required_version = ">= 1.14.0"
+
+  # These mirror your stack-level providers to help Core init in the prepare phase.
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
